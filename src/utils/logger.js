58e252af848c2bf53ba3a9587bdbefed5ecf47a6
Meet Chauhan <chauhanmeet1803@gmail.com>
2025-12-24 18:28:03 +0530

    feat: Update Winston logger utility and new dependencies for logging and API documentation.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,14 +1,18 @@
 import winston from "winston";
+import { consoleFormat } from "winston-console-format";
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || "info",
   format: winston.format.combine(
     winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
     winston.format.json()
   ),
+  defaultMeta: { service: "Test" },
   transports: [
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.File({ filename: "log/error.log", level: "error" }),
+    new winston.transports.File({ filename: "log/combined.log" }),
   ],
 });
 
@@ -16,8 +20,19 @@ if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
       format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
+        winston.format.colorize({ all: true }),
+        winston.format.padLevels(),
+        consoleFormat({
+          showMeta: true,
+          metaStrip: ["timestamp", "service"],
+          inspectOptions: {
+            depth: Infinity,
+            colors: true,
+            maxArrayLength: Infinity,
+            breakLength: 120,
+            compact: Infinity,
+          },
+        })
       ),
     })
   );
