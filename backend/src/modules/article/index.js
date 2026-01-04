1508e3ed52d1ee202f4c1aa27569d23ae258bd9e
Meet Chauhan <chauhanmeet1803@gmail.com>
2026-01-04 12:37:26 +0530

    chore: added barrel export for article module

---
+++ backend/src/modules/article/index.js
@@ -0,0 +1,6 @@
+export { default as Article } from "./models/Article.js";
+export { default as articleController } from "./article.controller.js";
+export { default as articleService } from "./article.service.js";
+export { default as articleRepository } from "./article.repository.js";
+export { default as articleRoutes } from "./article.routes.js";
+export { default as articleValidator } from "./article.validator.js";
